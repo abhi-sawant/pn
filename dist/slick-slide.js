2600d9d3ad2225663171fc598673d5ebbaedbc70
Abhishek <abhisawant111111@gmail.com>
2021-10-26 16:26:20 +0530

    animations

---
+++ dist/slick-slide.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('.hero').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+
+});
+
+$(document).ready(function () {
+    $('.standards__container').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+});
