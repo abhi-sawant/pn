ee19dc81ccd99c400aac1d917bb7a634839b5e29
Abhishek Sawant <abhisawant111111@gmail.com>
2022-01-02 11:13:45 +0530

    formatting

--- app/js/script.js
+++ app/js/script.js
@@ -1,37 +1,36 @@
-var ham = document.getElementById('hamburger');
-var navLinks = document.getElementById('navLinks');
+var ham = document.getElementById("hamburger");
+var navLinks = document.getElementById("navLinks");
 
 ham.addEventListener("click", function () {
-	if (navLinks.style.maxHeight) {
-		navLinks.style.maxHeight = null;
-		ham.style.width = "25px";
-	}
-	else {
-		navLinks.style.maxHeight = navLinks.scrollHeight + "px";
-		ham.style.width = "21px";
-	}
-})
+  if (navLinks.style.maxHeight) {
+    navLinks.style.maxHeight = null;
+    ham.style.width = "25px";
+  } else {
+    navLinks.style.maxHeight = navLinks.scrollHeight + "px";
+    ham.style.width = "21px";
+  }
+});
 
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function () {
-		this.classList.toggle("active");
-		var panel = this.nextElementSibling;
-		if (panel.style.maxHeight) {
-			// panel.style.padding = "0px";
-			panel.style.maxHeight = null;
-		} else {
-			// panel.style.padding = "1em";
-			panel.style.maxHeight = panel.scrollHeight + "px";
-		}
-	});
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      // panel.style.padding = "0px";
+      panel.style.maxHeight = null;
+    } else {
+      // panel.style.padding = "1em";
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
 
 const postApplied = document.querySelector("#postApplied");
 function carSel(carOption) {
-	postApplied.value = carOption;
+  postApplied.value = carOption;
 }
 
 // console.log(window.innerWidth);
@@ -45,5 +44,3 @@ function carSel(carOption) {
 // 		futureImg[i].style.marginTop = "-" + futureHeight + "px";
 // 	}
 // }
-
-
