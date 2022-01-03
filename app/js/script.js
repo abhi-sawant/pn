320b7975895bae29cf9cd350846ba3d1df40b5f5
Abhishek Sawant <abhisawant111111@gmail.com>
2022-01-03 08:47:42 +0530

    nav redesign

--- app/js/script.js
+++ app/js/script.js
@@ -1,36 +1,75 @@
-var ham = document.getElementById("hamburger");
-var navLinks = document.getElementById("navLinks");
+// var ham = document.getElementById("hamburger");
+// var navLinks = document.getElementById("navLinks");
 
-ham.addEventListener("click", function () {
-  if (navLinks.style.maxHeight) {
-    navLinks.style.maxHeight = null;
-    ham.style.width = "25px";
-  } else {
-    navLinks.style.maxHeight = navLinks.scrollHeight + "px";
-    ham.style.width = "21px";
-  }
-});
+// ham.addEventListener("click", function () {
+//    if (navLinks.style.maxHeight) {
+//       navLinks.style.maxHeight = null;
+//       ham.style.width = "25px";
+//    } else {
+//       navLinks.style.maxHeight = navLinks.scrollHeight + "px";
+//       ham.style.width = "21px";
+//    }
+// });
+
+let navbar = document.querySelector(".navbar");
+// let searchBox = document.querySelector(".search-box .bx-search");
+// // let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+// searchBox.addEventListener("click", ()=>{
+//   navbar.classList.toggle("showInput");
+//   if(navbar.classList.contains("showInput")){
+//     searchBox.classList.replace("bx-search" ,"bx-x");
+//   }else {
+//     searchBox.classList.replace("bx-x" ,"bx-search");
+//   }
+// });
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+// let moreArrow = document.querySelector(".more-arrow");
+// moreArrow.onclick = function() {
+//  navLinks.classList.toggle("show2");
+// }
+// let jsArrow = document.querySelector(".js-arrow");
+// jsArrow.onclick = function() {
+//  navLinks.classList.toggle("show3");
+// }
 
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      // panel.style.padding = "0px";
-      panel.style.maxHeight = null;
-    } else {
-      // panel.style.padding = "1em";
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
+   acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+         // panel.style.padding = "0px";
+         panel.style.maxHeight = null;
+      } else {
+         // panel.style.padding = "1em";
+         panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+   });
 }
 
 const postApplied = document.querySelector("#postApplied");
 function carSel(carOption) {
-  postApplied.value = carOption;
+   postApplied.value = carOption;
 }
 
 // console.log(window.innerWidth);
