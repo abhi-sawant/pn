ba9e1e5b9900bc2f9c7d4a8ccc2013b399400269
Abhishek Sawant <abhisawant111111@gmail.com>
2022-01-26 09:09:27 +0530

    mobile carousel

--- dist/slick-slide.js
+++ dist/slick-slide.js
@@ -1,73 +1,85 @@
 $(document).ready(function () {
-    $('.hero').slick({
-        dots: false,
-        arrows: false,
-        autoplay: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
+   $(".hero").slick({
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+   });
 });
 
 $(document).ready(function () {
-    $('.standards__container').slick({
-        dots: false,
-        arrows: false,
-        autoplay: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 450,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            },
-        ]
-    });
+   $(".hero-mobile").slick({
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+   });
 });
 
 $(document).ready(function () {
-    $('.success__carousels').slick({
-        dots: false,
-        arrows: false,
-        autoplay: true,
-        infinite: true,
-        speed: 3000,
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
+   $(".standards__container").slick({
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               slidesToShow: 3,
+               slidesToScroll: 1,
+            },
+         },
+         {
+            breakpoint: 600,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1,
+            },
+         },
+         {
+            breakpoint: 450,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+            },
+         },
+      ],
+   });
 });
 
 $(document).ready(function () {
-    $('.insights__container').slick({
-        dots: false,
-        arrows: false,
-        autoplay: true,
-        infinite: true,
-        speed: 3000,
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
+   $(".success__carousels").slick({
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      infinite: true,
+      speed: 3000,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+   });
+});
+
+$(document).ready(function () {
+   $(".insights__container").slick({
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      infinite: true,
+      speed: 3000,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+   });
 });
 
 // $(document).ready(function () {
@@ -83,13 +95,13 @@ $(document).ready(function () {
 // });
 
 $(document).ready(function () {
-    $('.carousel__container').slick({
-        dots: false,
-        arrows: false,
-        autoplay: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
+   $(".carousel__container").slick({
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+   });
 });
